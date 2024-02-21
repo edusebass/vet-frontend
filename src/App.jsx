86dfaef80d5a9fd524aee5c8967bbd9717e7076a
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-02-21 12:14:31 -0500

    MODAL VISUALIZAR

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import Perfil from './paginas/Perfil'
 import Restablecer from './paginas/Restablecer'
 import { AuthProvider } from './context/AuthProvider'
 import { PrivateRoute } from './routes/PrivateRoute'
+import { TratamientosProvider } from './context/TratamientosProvider'
 
 
 
@@ -24,37 +25,40 @@ function App() {
     <>
     <BrowserRouter>
     {/* import the auth porvider for verify token for login */}
-      <AuthProvider>
+      <TratamientosProvider>
 
-        <Routes>
-          
-          <Route index element={<LandinPage/>}/>
+        <AuthProvider>
 
-          <Route path='/' element={<Auth/>}>
-            <Route path='login' element={<Login/>}/>
-            <Route path='register' element={<Register/>}/>
-            <Route path='forgot/:id' element={<Forgot/>}/>
-            <Route path='confirmar/:token' element={<Confirmar/>}/>
-            <Route path='recuperar-password/:token' element={<Restablecer/>}/>
-            <Route path='*' element={<NotFound />} />
-          </Route>
+          <Routes>
+            
+            <Route index element={<LandinPage/>}/>
 
-          <Route path='dashboard/*' element={
-            <PrivateRoute>
-              <Routes>
-                <Route element={<Dashboard />}>
-                  <Route index element={<Perfil />} />
-                  <Route path='listar' element={<Listar />} />
-                  <Route path='visualizar/:id' element={<Visualizar />} />
-                  <Route path='crear' element={<Crear />} />
-                  <Route path='actualizar/:id' element={<Actualizar />} />
-                </Route>
-              </Routes>
-            </PrivateRoute>
-} />
+            <Route path='/' element={<Auth/>}>
+              <Route path='login' element={<Login/>}/>
+              <Route path='register' element={<Register/>}/>
+              <Route path='forgot/:id' element={<Forgot/>}/>
+              <Route path='confirmar/:token' element={<Confirmar/>}/>
+              <Route path='recuperar-password/:token' element={<Restablecer/>}/>
+              <Route path='*' element={<NotFound />} />
+            </Route>
 
-        </Routes>
-      </AuthProvider>
+            <Route path='dashboard/*' element={
+              <PrivateRoute>
+                <Routes>
+                  <Route element={<Dashboard />}>
+                    <Route index element={<Perfil />} />
+                    <Route path='listar' element={<Listar />} />
+                    <Route path='visualizar/:id' element={<Visualizar />} />
+                    <Route path='crear' element={<Crear />} />
+                    <Route path='actualizar/:id' element={<Actualizar />} />
+                  </Route>
+                </Routes>
+              </PrivateRoute>
+  } />
+
+          </Routes>
+        </AuthProvider>
+      </TratamientosProvider>
     </BrowserRouter>
     </>
   )
--- src/paginas/Visualizar.jsx
+++ src/paginas/Visualizar.jsx
@@ -1,12 +1,15 @@
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Mensaje from '../componets/Alertas/Mensaje';
+import ModalTratamiento from '../componets/Modals/ModalTratamiento';
+import { useContext, useEffect, useState } from 'react';
+import TratamientosContext from '../context/TratamientosProvider';
 
 const Visualizar = () => {
     const { id } = useParams()
     const [paciente, setPaciente] = useState({})
     const [mensaje, setMensaje] = useState({})
+    const {modal, handleModal} = useContext(TratamientosContext)
 
     const formatearFecha = (fecha) => {
         const nuevaFecha = new Date(fecha)
@@ -83,6 +86,12 @@ const Visualizar = () => {
                             </div>
                             <hr className='my-4' />
                             <p className='mb-8'>Este submódulo te permite visualizar los tratamientos del paciente</p>
+                            <div className='flex justify-between items-center'>
+                            <p>Este submódulo te permite visualizar los tratamientos del paciente</p>
+                                <button className="px-5 py-2 bg-green-800 text-white rounded-lg hover:bg-green-700" onClick={handleModal}>Registrar</button>
+                            </div>
+                            
+                            {modal && (<ModalTratamiento idPaciente={paciente._id} />)}
                             </>
                         )
                         :
