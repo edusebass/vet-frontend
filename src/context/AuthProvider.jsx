7a0647d8edf82a6f3a06d389a6934147722340f3
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-02-21 12:06:54 -0500

    modulo password

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -53,6 +53,24 @@ const AuthProvider = ({ children }) => {
         }
     }
 
+    const actualizarPassword = async (datos) => {
+        const token = localStorage.getItem('token')
+        try {
+            const url = `${import.meta.env.VITE_BACKEND_URL}/veterinario/actualizarpassword`
+            const options = {
+                headers: {
+                    method: 'PUT',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            }
+            const respuesta = await axios.put(url, datos, options)
+            return { respuesta: respuesta.data.msg, tipo: true }
+        } catch (error) {
+            return { respuesta: error.response.data.msg, tipo: false }
+        }
+    }
+
     //Fetch User Profile on Component Mount
     // The useEffect hook is used to fetch the user profile when the component is mounted ([] as dependency array).
     // It retrieves the token from the local storage and calls the perfil function if the token is present.
@@ -69,7 +87,8 @@ const AuthProvider = ({ children }) => {
             {
                 auth,
                 setAuth,
-                actualizarPerfil              
+                actualizarPerfil,
+                actualizarPassword              
             }
         }>
             {children}
